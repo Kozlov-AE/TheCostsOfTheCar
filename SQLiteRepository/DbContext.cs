6b43dcd51a83906337ba1d2f29d3168843cbee52
Aleksey <Kozlov-AE@users.noreply.github.com>
2020-08-25 19:17:15 +0300

    Добавьте файлы проекта.

---
+++ SQLiteRepository/DbContext.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace SQLiteRepository
+{
+    public class DbContext
+    {
+    }
+}
---
+++ SQLiteRepository/Entities/Car.cs
@@ -0,0 +1,28 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+
+namespace SQLiteRepository.Entities
+{
+    public class Car
+    {
+        public Car(string title)
+        {
+            Title = title;
+            BuyDate = DateTime.Now;
+            BuyPrice = 0;
+        }
+
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+        public DateTime BuyDate { get; set; }
+        public double BuyPrice { get; set; }
+
+        public virtual ICollection<Mileage> Mileages { get; set; }
+
+        //public virtual ICollection<Cost> CarCosts { get; set; }
+        //public virtual ICollection<Mileage> Mileages { get; set; }
+    }
+}
